9d23542ec5bafa16f1310c64dd58b97e75543502
FedericaC03 <fdrk03@hotmail.it>
2020-09-22 17:25:11 +0200

    Fine esercizio

--- script/script.js
+++ script/script.js
@@ -1,29 +1,32 @@
 $(document).ready(function(){
-searchClick();
 
-//CHIAMATA AJAX
-var searchMovie = value;
-$.ajax(
-  {
-    "url": "https://api.themoviedb.org/3/search/movie",
-    "data": {
-      "api_key": "639cb73214ae520200c0768191807286",
-      "query": searchMovie,
-      "language": "it-IT"
-    },
-    "method": "GET",
-    "success": function(data) {
-      var risultato = data.results;
-      renderMovie(risultato);
-      searchClick();
-    },
+//PRENDO IL VALORE DALLA INPUT QUANDO CLICCO SU CERCA
+  $("#search-button").click(function() {
+    var value = $("#search-bar").val();
+    //CHIAMATA AJAX
+    var searchMovie = value;
+    $.ajax(
+      {
+        "url": "https://api.themoviedb.org/3/search/movie",
+        "data": {
+          "api_key": "639cb73214ae520200c0768191807286",
+          "query": searchMovie,
+          "language": "it-IT"
+        },
+        "method": "GET",
+        "success": function(data) {
+          var risultato = data.results;
+          renderMovie(risultato);
+        },
 
-    "error": function(errore) {
-      alert("Errore");
-    }
-  }
-);
-
+        "error": function(errore) {
+          alert("Errore");
+        }
+      }
+    );
+    $("#search-bar").val(""); //svuoto la search bar dopo ogni invio
+    console.log(value);
+  });
 });
 
 
@@ -40,19 +43,8 @@ for (var i = 0; i < movies.length; i++) {
           "original_title" : movies[i].original_title ,
           "language" : movies[i].original_language,
           "vote" : movies[i].vote_average,
-
         };
         var html = template(context);
         $("#movie-list").append(html);
+  }
 }
-}
-
-//PRENDO IL VALORE DALLA INPUT QUANDO CLICCO SU CERCA
-function searchClick() {
-
-$("#search-button").click(function() {
-  var value = $("#search-bar").val("");
-  $("#search-bar").val(""); //svuoto la search bar dopo ogni invio
-  console.log(value);
-})
-};
